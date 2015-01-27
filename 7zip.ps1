0ed0f2ce0fc595cdeced78ca76e10917d7950c31
anurse <andrew@andrewnurse.net>
2015-01-27 09:59:51 -0800

    add 7zip recipe

---
+++ 7zip.ps1
@@ -0,0 +1,9 @@
+﻿Recipe 7zip {
+    version "9.38"
+    url "http://www.7-zip.org/a/7z938-x64.msi"
+
+    on install {
+        bin "Files\7-zip\7z.exe"
+        startmenu "Files\7-zip\7zFM.exe" "7-zip File Manager"
+    }
+}
